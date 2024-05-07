86d07fcc2955792a79082a3e59cdcf03bdb18848
Argxz <riyanargaw@gmail.com>
2024-05-07 22:01:55 +0700

    commit slider

--- js/script.js
+++ js/script.js
@@ -33,3 +33,25 @@ const menuList = document.getElementById("menu-list");
 menuIcon.addEventListener("click", ()=>{
     menuList.classList.toggle("hidden");
 })
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n){
+    showDivs(slideIndex+=n);
+}
+function showDivs(n){
+    var i;
+    var imgList= document.getElementsByClassName("img-slide");
+    if(n>imgList.length){slideIndex=1}
+    if (n<1){slideIndex=imgList.length};
+    for (i=0; i<imgList.length; i++) {
+        imgList[i].style.display = "none";
+    }
+    imgList[slideIndex-1].style.display = "block";
+}
+
+setInterval(()=>{
+    plusDivs(1);
+},1200);
